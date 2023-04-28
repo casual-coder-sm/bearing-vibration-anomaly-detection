6d5d2119e15166003e701d46365167c75e0e96ff
casual-coder-sm <86151745+casual-coder-sm@users.noreply.github.com>
2023-04-28 18:37:16 +0000

    Device status display streamlit

--- gui/streamlit_show_device_anomaly_status.py
+++ gui/streamlit_show_device_anomaly_status.py
@@ -5,9 +5,11 @@ import pandas as pd
 
 from io import BytesIO
 from pathlib import Path
+import time
 
 from time import perf_counter
 
+st.title('IoT Device Live Status')
 
 def color_status(val):
     color = '#AAFF00' if val == 'Operational' else '#FF5733'
@@ -15,8 +17,8 @@ def color_status(val):
 
 
 def status(val):
-    text = 'Operational' if val else 'Error'
-    color = '#AAFF00' if val else '#FF5733'
+    text = 'Operational' if val==0 else 'Error'
+    color = '#AAFF00' if val==0 else '#FF5733'
     return f'<p style="background:{color}; width:30px; height:30px; border-radius:50%;text-indent: -9999px;">{text}</p>'
 
 
@@ -31,23 +33,33 @@ for s3_bucket_object in s3_bucket.objects.filter(Prefix='data_ingested/device_st
     s3_bucket_objects.append(s3_bucket_object)
 
 s3_device_objects={}
-s3_csv_object=''
+s3_csv_object=None
 for s3_object in s3_bucket_objects:
     path_parts = Path(s3_object.key).parts
     if len(path_parts) == 3 and '.csv' in path_parts[2]:
         s3_csv_object=s3_object
+        print(s3_csv_object)
 
 # Retrieve file contents.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=5)
-def get_device_live_status(s3_csv_object):
+@st.cache_data(ttl=2)
+def get_device_live_status(_s3_csv_object):
     df = pd.DataFrame()
-    data = s3_csv_object.get()['Body'].read()
+    data = _s3_csv_object.get()['Body'].read()
     df = pd.read_csv(BytesIO(data), index_col=[0])
+    print(df)
+    df = df.rename(columns={'devicename':'Device-Name','filename':'Date-Time'})
     df['Anomaly']=df['Anomaly'].apply(status)
     df = df.to_html(escape=False)
+    return df
 
-df = get_device_live_status(s3_csv_object)
-st.write(df, unsafe_allow_html=True)
+if s3_csv_object is not None:
+    df = get_device_live_status(s3_csv_object)
+    st.write(df, unsafe_allow_html=True)
+    time.sleep(2)
+    st.experimental_rerun()
+else:
+    time.sleep(5)
+    st.experimental_rerun()
 
 
